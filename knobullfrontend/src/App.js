4a6651720ee7ed9ef8928ff0a9b1f73a0d02e016
Peter Ong <onggiaphuoc2004@gmail.com>
2023-03-13 14:47:26 -0400

    Peter Update

--- knobullfrontend/src/App.js
+++ knobullfrontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import StudentCard from './Card/StudentCard';
 
 function App() {
   return (
       <div className="App">
           <h1>Header</h1>
     <h1>News Articles Go Here</h1>
+    <StudentCard></StudentCard>
     </div>
   );
 }
---
+++ knobullfrontend/src/Card/StudentCard.js
@@ -0,0 +1,15 @@
+
+function StudentCard() {
+    return (
+        <div class="card-p-3" style={{width: '25rem'}}>
+            <img class="card-img-top" src="https://www.collegiateparent.com/wp-content/uploads/2022/08/AdobeStock_429176281.jpg" alt="Card image cap"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    );
+}
+
+export default StudentCard;
