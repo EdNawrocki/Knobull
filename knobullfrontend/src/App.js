c0bfdad8da04f079433661312a70c48a746c9f0f
Peter Ong <onggiaphuoc2004@gmail.com>
2023-03-16 16:55:17 -0400

    Peter submit 03/16/2023

--- knobullfrontend/src/App.js
+++ knobullfrontend/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
-import StudentCard from './Card/StudentCard';
+import StudentCard from './Components/StudentCard';
+import NavBar from './Components/NavBar'
+import ListMediaObjects from './Components/ListMediaObjects';
+
 
 function App() {
   return (
-      <div className="App">
-          <h1>Header</h1>
-    <h1>News Articles Go Here</h1>
-    <StudentCard></StudentCard>
+    <div>
+      <NavBar/>
+      <div className="container App">  {/* use container from Bootstrap */}
+        <h1>Header</h1>
+        <h1>News Articles Go Here</h1>
+        <div className="row">
+          <div className="col-8">
+            <StudentCard></StudentCard>
+            <hr></hr>
+            <h2>The Latest</h2>
+            <ListMediaObjects/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ knobullfrontend/src/Components/NavBar.js
@@ -0,0 +1,22 @@
+
+
+function NavBar() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><img style={{width:"50px"}} src="https://media.licdn.com/dms/image/C560BAQHxeMRytUU5xw/company-logo_200_200/0/1601089066813?e=2147483647&v=beta&t=ajOmaM3gWC9JeOQZbKk59r0GtfZ8Gzv1cGyVpEd3534"/></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">News</a>
+                    <a class="nav-item nav-link" href="#">Blog</a>
+                    <a class="nav-item nav-link" href="#">About Us</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
